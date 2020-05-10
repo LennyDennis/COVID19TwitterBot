0fc31c4de2db684616284c2168130a5a91514f4e
LennyDennis <lennydennis@gmail.com>
2020-05-10 11:54:31 +0300

    Add image to base64 dependency

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ let T = new Twit(config);
 tweetUpdate();
 setInterval(tweetUpdate(), 1000*60*60*6  )
 
+function tweetUpdate(){
+    let imagePath = path.join(__dirname, '/images/komesha.jpg');
+    let b64content = fs.readFileSync(imagePath,{encoding:'base64'});
+
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {    
+        let mediaIdStr = data.media_id_string
+        let altText = "Small flowers in a planter on a sunny balcony, blossoming."
+        let meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+       
+        T.post('media/metadata/create', meta_params, function (err, data, response) {
+          if (!err) {
+            let tweet = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
+       
+            T.post('statuses/update', tweet, function (err, data, response) {
+              console.log(data)
+            })
+          }
+        })
+    })
+    
+
+}
