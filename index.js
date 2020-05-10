4386cfcbcbdd062e2f5ab5c9bc2b5f49692c7452
LennyDennis <lennydennis@gmail.com>
2020-05-10 11:44:37 +0300

    Add the media upload functionality in the tweetUpdate function

--- index.js
+++ index.js
@@ -6,19 +6,3 @@ let T = new Twit(config);
 tweetUpdate();
 setInterval(tweetUpdate(), 1000*60*60*6  )
 
-function tweetUpdate(){
-
-let tweet = {
-    status:'Jambo Kenyans, I am COVID19 Kenya bot that tweets the latest info about the coronavirus every 12 hours.'
-}
-
-T.post('statuses/update', tweet, tweeted)
-
-function tweeted(err,data, response){
-    if(err){
-        console.log("err")
-    }else{
-        console.log("hello")
-    }
-  }
-}
