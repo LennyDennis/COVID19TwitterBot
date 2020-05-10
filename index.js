a9575ca8dde342bd876596a83c256d00caaf96bb
LennyDennis <lennydennis@gmail.com>
2020-05-10 14:51:21 +0300

    Include novelcovid API that updates on cases in the worl and kenya

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-let Twit = require('twit');
-let config = require('./config');
-let fs = require('fs');
-let path = require('path')
+const Twit = require('twit');
+const config = require('./config');
+const fs = require('fs');
+const path = require('path');
+const { NovelCovid } = require("novelcovid");
 
 let T = new Twit(config);
 
 tweetUpdate();
-// setInterval(tweetUpdate(), 1000*60*60*6  )
+setInterval(tweetUpdate, 1000*10)
 
-function tweetUpdate(){
-    let imagePath = path.join(__dirname, '/images/komesha.jpg');
+async function tweetUpdate(){
+    const track = new NovelCovid();
+    const imagePath = path.join(__dirname, '/images/komesha.jpg');
     let b64content = fs.readFileSync(imagePath,{encoding:'base64'});
 
-    T.post('media/upload', { media_data: b64content }, function (err, data, response) {    
-        let mediaIdStr = data.media_id_string
+    let worldStats = await track.all(); 
+    let kenyaStats = await track.countries('Kenya');
+
+    function deathtitle(){
+        if(kenyaStats.todayDeaths>1){
+            return 'Today\'s Deaths'
+        }else{
+            return 'Today\'s Death'
+        }
+    }
+
+    let worldUpdate = `🌍 Worldwide COVID19 Statistics\nConfirmed Cases: ${worldStats.cases.toLocaleString()} (+${worldStats.todayCases.toLocaleString()})\nRecovered: ${worldStats.recovered.toLocaleString()}\nDeaths: ${worldStats.deaths.toLocaleString()} (+${worldStats.todayDeaths})`;
+    let kenyaUpdate = `\n\n🇰🇪  Kenya COVID19 Statistics:\nConfirmed Cases: ${kenyaStats.cases.toLocaleString()} (+${kenyaStats.todayCases.toLocaleString()})\nRecovered: ${kenyaStats.recovered.toLocaleString()}\nDeaths: ${kenyaStats.deaths.toLocaleString()} (+${kenyaStats.todayDeaths})\nTotal Tests: ${kenyaStats.tests.toLocaleString()}`;
+    let hashtags = `\n\n#KomeshaCorona #covid_19Ke #covid19`
+ 
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {        let mediaIdStr = data.media_id_string
         let altText = "Small flowers in a planter on a sunny balcony, blossoming."
         let meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
        
         T.post('media/metadata/create', meta_params, function (err, data, response) {
           if (!err) {
-            let tweet = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
+            let tweet = { status: worldUpdate + kenyaUpdate + hashtags, media_ids: [mediaIdStr] }
        
             T.post('statuses/update', tweet, function (err, data, response) {
-              console.log(data)
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log('Tweet successful')
+                }
             })
           }
         })
