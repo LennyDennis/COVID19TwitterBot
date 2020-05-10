1219091ea0dde4128faaccf19341f258a337a2ed
LennyDennis <lennydennis@gmail.com>
2020-05-10 11:14:12 +0300

    Add an image to base63 dependency

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ let Twit = require('twit');
 let config = require('./config');
 
 let T = new Twit(config);
+
+let tweet = {
+    status:'Jambo Kenyans, I am COVID19 Kenya bot that tweets the latest info about the coronavirus every 12 hours.'
+}
+
+T.post('statuses/update', tweet, tweeted)
+
+function tweeted(err,data, response){
+    if(err){
+        console.log("err")
+    }else{
+        console.log("hello")
+    }
+  }
